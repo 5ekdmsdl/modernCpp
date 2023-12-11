f6730a3cf44bb2cde3061972fb8973eb305981bd
5ekdmsdldl <5ekdmsdldl@gmail.com>
2023-12-11 14:47:54 +0900

    override 1

--- 14_override.cpp
+++ 14_override.cpp
@@ -1,23 +1,32 @@
 // 14_override  - 44 page
 
-class Base
-{
+class Base {
 public:
 	virtual void foo() {}
 	virtual void goo(int a) {}
 	virtual void hoo() {}
 };
 
-class Derived : public Base
-{
+class Derived : public Base {
 public:
-	// 가상 함수 재정의시 실수(오타)
-	virtual void fooo() {}
+	// 가상 함수 재정의시 실수(오타) 발생해도 컴파일 에러 X
+	// => 컴파일러 : 그냥 새로 만들었나보다~~
+	/*virtual void fooo() {}
 	virtual void goo(double a) {}
 	virtual void hoo() const  {}
+	*/
+	
+	// => Cpp11 부터 override 키워드 제공!
+	// compiler가 error 찾아준다.
+	virtual void fooo() override {}
+	virtual void goo(double a) override  {}
+	virtual void hoo() const  override {}
+	
+	// => 가상함수 재정의시에 override 필수!!
 };
 
+int main() {
 
-int main()
-{
+
+
 }
