19ae023c42c231b54d7bd5cab8e9904bb8c50100
5ekdmsdldl <5ekdmsdldl@gmail.com>
2023-12-11 10:59:16 +0900

    begin: string view

--- 4_beginend.cpp
+++ 4_beginend.cpp
@@ -1,7 +1,9 @@
 // 4_beginend - 16 page
 #include <vector>
 #include <list>
-#include <iostream>
+#include <iostream>#
+#include <string>
+#include <string_view> // C++17!!
 
 int main()
 {
@@ -59,6 +61,25 @@ int main()
 
 	///////////////////////////////////////////////////////////////
 
+	// std::string      : 자원(문자열을 소유) 합니다
+	// std::string_view : 자원을 소유하지 않고, 빌려서 사용합니다. (C17 부터)
+	std::string s = "to be or not to be";
+	std::string_view sv = s;
+	// sv는 파괴되도 s 멀쩡
 
 
+	// 아래 코드를 생각해 보세요
+	auto p1 = std::ranges::begin(std::vector<int>{1, 2, 3});
+	// vector는 자원을 소유한 컨테이너 -> 자원소유하는 컨테이너의 임시객체 전달하면 error
+	auto p2 = std::ranges::begin(std::string_view{ s });
+	// 근데 빌려쓰는 컨테이너는 임시객체여도 error X
+	// 반복자는 결국 자원의 소유자를 가르킨다.
+
+	// C20 : 자원 소유 컨테이너 vs 빌려쓰는 컨테이너 중요하게 된다.
+	//         자원을 빌려서 사용하는 컨테이너 인지가 중요하게 됩니다.
+	//		   "borrowed range" 용어가 공식적으로 등장하게 됩니다.
+
+	// int& : 가리킬수만 있습니다.(내부 원리는 포인터)
+	// borrowed container : 내부 원리 ( 포인터 + 갯수 입니다.)
+	//						=> RUST 등에서는 "FAT POINTER" 라고도 합니다.
 }
