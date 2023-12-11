908d75c5a2e9e4ab80bbdf450ef759d692f2cfdd
5ekdmsdldl <5ekdmsdldl@gmail.com>
2023-12-11 11:41:08 +0900

    Inherite Constructor

--- 7_생성자상속1.cpp
+++ 7_생성자상속1.cpp
@@ -6,9 +6,16 @@ public:
 	Base()      : value(0) {}
 	Base(int n) : value(n) {}
 };
+
+// 생성자는 상속되지 X
 class Derived : public Base
 {
 public:
+	//C11 부터는 생성자 상속 가능
+	// => using 클래스이름::생성자이름 
+	using Base::Base;
+	// Base(), Base(int) 모두 상속되는 것
+	
 };
 
 int main()
